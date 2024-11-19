af4d323c84e97b86d065ded295247c37da060956
Franlop24 <franlopbri@gmail.com>
2024-11-19 13:59:24 -0600

    Header, Footer and Guitarra Compoments

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import { ref, reactive } from 'vue'
+  import { db } from './data/guitarras'
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+  import Guitarra from './components/Guitarra.vue'
+
+  const guitarras = ref(db)
+
+</script>
+
+<template>
+   <Header />
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+        <div class="row mt-5">
+            <Guitarra 
+                v-for="guitarra in guitarras"
+                :guitarra="guitarra"
+                :key="guitarra.id"
+            />
+        </div>
+    </main>
+    <Footer />
+</template>
+
+<style scoped>
+
+</style>
